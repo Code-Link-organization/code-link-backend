c944a392e54c88bd291c2469e05c0ddb251689e4
eman2351799 <124907595+eman2351799@users.noreply.github.com>
2023-09-01 23:43:07 +0200

    Mentor Controller

---
+++ app/Http/Controllers/Api/MentorController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MentorController extends Controller
+{
+    
+    public function index()
+    {
+        
+    }
+
+    public function createMentor(Request $request)
+    {
+       
+    }
+
+   
+    
+
+    
+    public function showMentor($id)
+    {
+        
+    }
+
+    public function editMentor(Request $request, $id)
+    {
+        
+    }
+
+   
+
+    public function destroyMentor($id)
+    {
+        
+    }
+}
