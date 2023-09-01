74dd32af14e7b27cc1f7d5aa3eb72b0c647c71b4
eman2351799 <124907595+eman2351799@users.noreply.github.com>
2023-09-01 22:34:19 +0200

    Search Controller

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+
+    public function searchPost($id)
+    {
+    }
+
+
+    public function searchTeam($id)
+    {
+    }
+
+
+    public function searchMentor($id)
+    {
+    }
+
+
+    public function searchCommunity($id)
+    {
+    }
+
+    public function searchUserprofile($id)
+    {
+    }
+
+
+    public function searchChat($id)
+    {
+    }
+
+
+    public function searchNotification($id)
+    {
+    }
+}
