965a9ece4d5550f636967385d30d408f317a6019
EmanElhelaly <emanelhelaly11@gmail.com>
2023-09-16 09:33:10 +0200

    Test User Registration

---
+++ tests/Feature/SignupControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class SignupControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /**
+     * Test user registration and email verification.
+     *
+     * @return void
+     */
+    public function testUserRegistration()
+    {
+        $userData = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => 'password123',
+            'password_confirmation' => 'password123', 
+        ];
+    
+        $response = $this->postJson('/api/user/signup', $userData);
+    
+        $response->assertStatus(200);
+    
+        $this->assertDatabaseHas('users', ['email' => $userData['email']]);
+        $user = User::where('email', $userData['email'])->first();
+        $this->assertTrue(Hash::check('password123', $user->password));
+    }
+    
+}
